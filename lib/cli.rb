fe62f5035d20a2672de7e3bb8cd68ec869c8e8ef
Ernie Brodeur <ebrodeur@ujami.net>
2012-04-14 17:08:36 -0700

    Initial commit of a cli model, works, but doesn't do anything beyond parsing.

---
+++ lib/cli.rb
@@ -0,0 +1,18 @@
+# This library is to reduce the amount of effort I need to build a binary with option parsing
+# and sensable defaults.  It is not intended to be convient for anybody but me, their
+# are a host of good options out there if you want cli tools.
+
+require 'optparse'
+require 'ostruct'
+
+### Options Block
+Options = OpenStruct.new
+Options.verbose = false
+Options.version = false
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: Use ruby to cat a file and produce colored output."
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") { |verbose| Options.verbose = v }
+  opts.on("-V", "--version", "Print version") { |version| Options.version = true}
+end.parse!
