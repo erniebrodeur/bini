f9d454b7ce993475892bdcafce3a33fd4bec2476
Ernie Brodeur <ebrodeur@ujami.net>
2012-04-14 17:39:27 -0700

    Changed around the cli to provide an Options via OptBlob.
    The OptBlob has a bit of extra functionality, it is both the options
    array and the options parser wraped into one object.  It provides
    default options for verbose and version, including exiting when version
    is called.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,23 +1,53 @@
 # This library is to reduce the amount of effort I need to build a binary with option parsing
 # and sensable defaults.  It is not intended to be convient for anybody but me, their
-# are a host of good options out there if you want cli tools.
+# are a host of good @options out there if you want cli tools.
 
 require 'optparse'
 require 'ostruct'
 
-### Options Block
-Options = OpenStruct.new
-Options.verbose = false
-Options.version = false
+class OptBlob
+  def initialize
+    @options = {}
+    @options[:verbose] = false
+    @options[:version] = false
 
-OptionParser.new do |opts|
-  opts.banner = Banner
+    @parser = OptionParser.new do |opts|
+      opts.banner = Banner
 
-  opts.on("-v", "--[no-]verbose", "Run verbosely") { |verbose| Options.verbose = v }
-  opts.on("-V", "--version", "Print version") { |version| Options.version = true}
-end.parse!
+      opts.on("-v", "--[no-]verbose", "Run verbosely") { |verbose| @options[:verbose] = v }
+      opts.on("-V", "--version", "Print version") { |version| @options[:version] = version}
+    end
+  end
 
-if Options.version
-	puts Version
-	exit 0
+  #TODO figure out a more dynamic way to include the parser and hash table.
+  def []=(item)
+    @options[item]
+  end
+
+  def [](item)
+    @options[item]
+  end
+
+  def on(*opts, &block)
+  	@parser.on(*opts, &block)
+  end
+
+  def on_tail(*opts, &block)
+  	@parser.on(*opts, &block)
+  end
+
+  def on_head(*opts, &block)
+  	@parser.on(*opts, &block)
+  end
+
+ 	def parse!
+ 		@parser.parse!
+
+    if @options[:version]
+      puts Version
+      exit 0
+    end
+ 	end
 end
+
+Options = OptBlob.new
