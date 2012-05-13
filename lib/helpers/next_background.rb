e509442236526664c8698ac0d6d16b98592a7666
Ernie Brodeur <ebrodeur@ujami.net>
2012-05-13 00:40:10 -0700

    Stubbed out a next_background app to mimic my old one.

---
+++ lib/helpers/next_background.rb
@@ -0,0 +1,18 @@
+module ErnieBrodeur
+	module NextBackground
+		def add_directory(dir)
+		end
+
+		def add_file(file)
+		end
+
+		def run_once
+		end
+
+		def output_file
+		end
+
+		def daemonize
+		end
+	end
+end
