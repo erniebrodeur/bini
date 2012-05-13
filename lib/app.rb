cd25b0bbbfba395827ce8af805b963d334f6488e
Ernie Brodeur <ebrodeur@ujami.net>
2012-05-12 22:01:42 -0700

    Fixed a bug where the DB didn't work because the app name was an absolute path.

--- lib/app.rb
+++ lib/app.rb
@@ -4,6 +4,12 @@ module ErnieBrodeur
 		attr_accessor :banner
 		attr_accessor :long_description
 
+		# return the name of the app, for now this is just the cmd ran, later it will be
+		# something generated but more unique.
+		def name
+			$0.split("/").last
+		end
+
 		def initialize
 			@version = '0.0.0'
 			@banner  = 'A bin snippet by Ernie Brodeur that does . . . something.'
