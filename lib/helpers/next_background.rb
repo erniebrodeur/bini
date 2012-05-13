436f076a89fd19482cf49b0096f5a14b91ff7976
Ernie Brodeur <ebrodeur@ujami.net>
2012-05-13 02:20:40 -0700

    Filled out the stubs, so now they do stuff.

--- lib/helpers/next_background.rb
+++ lib/helpers/next_background.rb
@@ -1,9 +1,24 @@
 module ErnieBrodeur
 	module NextBackground
 		def add_directory(dir)
+			Dir.glob("#{dir}**/*").each do |file|
+				add_file file
+			end
 		end
 
 		def add_file(file)
+			# does it exist in the data base? if so return it
+			if (f = ErnieBrodeur::Models::Image.find(file))
+				puts "File existed: #{file}"
+				return f
+			end
+			# Is our file an image?  if not, just return nil.
+			return nil if !ErnieBrodeur::is_image?(file)
+			puts "Adding file: #{file}"
+			begin
+				ErnieBrodeur::Models::Image.create!(:filename => file, :md5sum => true)
+			rescue
+			end
 		end
 
 		def run_once
