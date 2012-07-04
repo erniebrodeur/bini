7d11c888f5feb3b8dbd7e835d034f44676d1659c
Ernie Brodeur <ebrodeur@ujami.net>
2012-07-04 01:37:17 -0700

    The stub of my logger.

---
+++ lib/log.rb
@@ -0,0 +1,14 @@
+# It should contain a configuration array.
+# Whenever changes are made to the configuration array, updates should be applied.
+# It should have a semi easy to read standard format
+# it should produce colorized output (either parsed or part of the file format)
+# it should wrap logger.
+
+module ErnieBrodeur
+	class Logger
+		def initalize
+		end
+
+	end
+	Log = Logger.new
+end
