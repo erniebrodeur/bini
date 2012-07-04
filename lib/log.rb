f6c9b1c69d9f9a58c5bd451629f8d24af6a47c4b
Ernie Brodeur <ebrodeur@ujami.net>
2012-07-04 03:33:16 -0700

    Made it UTC, edited the notes on top.

--- lib/log.rb
+++ lib/log.rb
@@ -1,18 +1,16 @@
 # It should contain a configuration array.
 # Whenever changes are made to the configuration array, updates should be applied.
-# it should produce colorized output (either parsed or part of the file format)
-# it should wrap logger.
 # it should override puts and print to 'capture' output as debug output.
-# it will log in UTC by default
 # It should have a semi easy to read standard format
 # it will have multiple formats available.
+# it should produce colorized output (either parsed or part of the file format)
 module ErnieBrodeur
   class Logger
     def initialize
       @l = ::Logger.new(STDOUT)
 
       @l.formatter = proc do |severity, datetime, progname, msg|
-        "[#{datetime.asctime}] [#{severity}]: #{msg}\n"
+        "[#{datetime.getutc.asctime}] [#{severity}]: #{msg}\n"
       end
     end
 
