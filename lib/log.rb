c8d85f76027278bc3f38d8764d2627e5a6cc7707
Ernie Brodeur <ebrodeur@ujami.net>
2012-07-04 02:20:31 -0700

    Added a basic formatter, added []= to be level/msg

--- lib/log.rb
+++ lib/log.rb
@@ -1,14 +1,32 @@
 # It should contain a configuration array.
 # Whenever changes are made to the configuration array, updates should be applied.
-# It should have a semi easy to read standard format
 # it should produce colorized output (either parsed or part of the file format)
 # it should wrap logger.
-
+# it should override puts and print to 'capture' output as debug output.
+# it will log in UTC by default
+# It should have a semi easy to read standard format
+# it will have multiple formats available.
 module ErnieBrodeur
-	class Logger
-		def initalize
-		end
+  class Logger
+    def initialize
+      @l = ::Logger.new(STDOUT)
 
-	end
-	Log = Logger.new
+      @l.formatter = proc do |severity, datetime, progname, msg|
+        "[#{datetime.asctime}] [#{severity}]: #{msg}\n"
+      end
+    end
+
+    # key is the log level and value is the msg to be logged.
+    def []=(key, value)
+      case key
+      	when :f || :fatal || /[fatal]/i then @l.fatal value
+      	when :e || :error || /[error]/i then @l.error value
+      	when :w || :warn  || /[warn]/i then @l.warn value
+      	when :i || :info  || /[info]/i then @l.info value
+      	when :d || :debug || /[debug]/i then @l.debug value
+     	end
+    end
+  end
+
+  Log = ErnieBrodeur::Logger.new
 end
