b394f1f444b3430b16aaaf1b8dfdf06ed074def7
Ernie Brodeur <ebrodeur@ujami.net>
2012-07-09 02:39:59 -0700

    Updated OptBlob to a subclass of ::Optparaser.

---
+++ lib/optparser.rb
@@ -0,0 +1,53 @@
+require 'optparse'
+
+module ErnieBrodeur
+  class OptionParser < ::OptionParser
+    def initialize
+      @options = {}
+      super
+
+      on("-V", "--version", "Print version") { |version| @options[:version] = true}
+      on("-p", "--pry", "open a pry shell.") { |pry| @options[:pry] = true}
+      if App.plugins.include? 'logging'
+        on("-l", "--log-level LEVEL", "Change the log level, default is debug.") { |level| ErnieBrodeur::Log.level level }
+        on("--log-file FILE", "What file to output to, default is STDOUT") { |file| ErnieBrodeur::Log.filename file }
+      end
+    end
+
+    # This will build an on/off option with a default value set to false.
+    def bool_on(word, description = "")
+      Options[word.to_sym] = false
+      on "-#{word.chars.first}", "--[no]#{word}", description  do |o|
+        Options[word.to_sym] == o
+      end
+    end
+
+    def parse!
+      @banner = ErnieBrodeur::App.banner
+      super
+
+      if @options[:version]
+        puts ErnieBrodeur::App.version
+        exit 0
+      end
+
+      # we need to mash in our config array.  To do this we want to make config
+      # options that don't overwrite cli options.
+      if App.plugins.include? 'config'
+        Config.each do |k,v|
+          @options[k] = v if !@options[k]
+        end
+      end
+    end
+
+    def on_pry
+      if @options[:pry]
+        require 'pry'
+        binding.pry
+      end
+    end
+  end
+
+  App.plugins.push "optparser"
+  Options = OptionParser.new
+end
