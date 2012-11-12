6b212a02805978f03790f159a0f01a46dce11a09
Ernie Brodeur <ebrodeur@ujami.net>
2012-11-11 23:23:21 -0800

    Test my keys, even if it is redundant (whats 1 or 2 msec's between friends.)

--- spec/lib/bini/bini_spec.rb
+++ spec/lib/bini/bini_spec.rb
@@ -21,82 +21,30 @@ describe "Bini" do
 			Bini.parameters?.should be_true
 		end
 	end
-	# I can probably do this dynamically.
-	describe "long_name" do
-		it "can be reset" do
-			Bini.long_name = nil
-			Bini.long_name.should eq Bini.defaults[:long_name]
-		end
 
-		it "provides a default" do
-			Bini.long_name = nil
-			Bini.long_name.should_not be_nil
-		end
+	# Dynamically generate our key tests, more for fun then any real need.
+	Bini.keys.each do |key|
+		describe "#{key}" do
+			it "can be reset to a default" do
+				Bini.send "#{key}=", nil
+				Bini.send(key).should eq Bini.defaults[key]
+			end
+			it "can be overriden" do
+				r = random_hex 16
+				Bini.send "#{key}=", r
+				Bini.send(key).should eq r
+			end
 
-		it "can be overridden" do
-			r = random_hex 4
-			Bini.long_name = r
-			Bini.long_name.should eq r
-		end
-
-		it "can be configured via .configure" do
-			r = random_hex 4
+			it "can be configured via .configure" do
+				r = random_hex 4
 				Bini.configure do |c|
-					c.long_name = r
+					c.instance_variable_set "@#{key}", r
 				end
-			Bini.long_name.should eq r
-		end
-	end
-
-	describe "cache_dir" do
-		it "can be reset" do
-			Bini.cache_dir = nil
-			Bini.cache_dir.should eq @cache_dir
-		end
-
-
-		it "provides a default" do
-			Bini.cache_dir = nil
-			Bini.cache_dir.should_not be_nil
-		end
-
-		it "can be overridden" do
-			r = random_hex 4
-			Bini.cache_dir = r
-			Bini.cache_dir.should eq r
-		end
-		it "can be configured via .configure" do
-			r = random_hex 4
-			Bini.configure do |c|
-				c.cache_dir = r
+				Bini.send(key).should eq r
 			end
-			Bini.cache_dir.should eq r
 		end
 	end
-	describe "config_dir" do
-		it "can be reset" do
-			Bini.config_dir = nil
-			Bini.config_dir.should eq @config_dir
-		end
 
-
-		it "provides a default" do
-			Bini.config_dir = nil
-			Bini.config_dir.should_not be_nil
-		end
-
-		it "can be overridden" do
-			r = random_hex 4
-			Bini.config_dir = r
-			Bini.config_dir.should eq r
-		end
-		it "can be configured via .configure" do
-			r = random_hex 4
-			Bini.configure do |c|
-				c.config_dir = r
-			end
-			Bini.config_dir.should eq r
-		end
-	end
+	Bini.version
 end
 
