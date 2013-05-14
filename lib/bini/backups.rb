dc81c25c1919454ece6857322eb3fe697ba564d9
Ernie Brodeur <ebrodeur@ujami.net>
2013-05-13 18:11:08 -0700

    Added support for saving/storing filemode.

--- lib/bini/backups.rb
+++ lib/bini/backups.rb
@@ -14,46 +14,58 @@ module Bini
       return Digest::SHA256.hexdigest(open(filename).read)
     end
 
-
+    # this is slightly better then restore, but still not very clear.
     def store(filename)
       hex = generate_key(filename)
       hex_file = "#{Bini.backup_dir}"
+
       index[hex] = Array.new if !index[hex]
-      index[hex] << filename if !index[hex].include? filename
+      index[hex] << [filename, File.stat(filename).mode] if !index[hex].flatten.include? filename
 
       FileUtils.mkdir_p Bini.backup_dir
       FileUtils.cp "#{filename}", "#{Bini.backup_dir}/#{hex}"
+
       index.save
     end
 
+    # this is just not elegant.
     def restore(filename)
-      raise "ohShit" if duplicate_hex? filename
+      raise "ohShit, it appears the index has two sha256sums for one filename." if duplicate_hex? filename
 
-      hex = index.select {|k,v| v.include? filename }.keys.first
-      return false if !index[hex] || !index[hex].include?(filename)
+      hex = get_hex filename
+      return false if !check_hex(filename, hex)
+
       restore_file = "#{Bini.backup_dir}/#{hex}"
+      FileUtils.cp restore_file, filename
 
-      current_hex = generate_key(filename)
-      return false if current_hex && current_hex != hex
+      mode = index[hex].select {|i| i[0] == filename}.first.last
+      FileUtils.chmod mode, restore_file
 
-      FileUtils.cp restore_file, filename, verbose:true
-      index[hex].delete filename
+      index[hex].delete_if {|i| i[0] == filename}
       index.delete_if {|k,v| k == hex} if index[hex].empty?
       index.save
     end
 
-    def files(hex)
-      index[hex]
-    end
-
     def duplicate_hex?(filename)
-      results = index.select {|k,v| v.include? filename }
+      results = index.select {|k,v| v.flatten.include? filename }
       return true if results.size > 1
       return false
+    end
+
+    def get_hex(filename)
+      index.select {|k,v| v.flatten.include? filename }.keys.first
     end
 
     def stored?(filename)
       index.include? generate_key filename
     end
+
+    def check_hex(filename, hex)
+      return false if !index[hex] || !index[hex].flatten.include?(filename)
+      restore_hex = generate_key(filename)
+      return false if restore_hex && restore_hex != hex
+      return true
+    end
   end
 end
+
